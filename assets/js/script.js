18e29d800261bf56fdb238bba5d7f0a2ad3860c6
Williamatthewood <w.wood@tcu.edu>
2023-04-02 18:19:50 -0500

    main fetch requests working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,48 @@ var currentDate = document.getElementById("current-date");
 var currentTemp = document.getElementById("current-temp");
 var currentWind = document.getElementById("current-wind");
 var currentHumidity = document.getElementById("current-humidity");
+var searchButton = document.getElementById("search-btn");
+var citySearchText = document.getElementById("city-search");
 
-function getAPI(request){
+var currentLat;
+var currentLon;
+
+function getAPIData(request){
     fetch(request).then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data);
+        console.log(data[0].lat, data[0].lon);
+        currentLat = data[0].lat;
+        currentLon = data[0].lon;
+        console.log(currentLat, currentLon);
+        var newAPICall = "https://api.openweathermap.org/data/2.5/forecast?lat=" + currentLat + "&lon=" + currentLon + "&appid=f4ce1d986e6fe51c9fed10dd756f803b&units=imperial";
+        fetch(newAPICall).then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data, data.list[0]);
+        })
     })
 }
 
-getAPI(requestURL);
+function searchForCity(event){
+    event.preventDefault();
+    var currentCity = citySearchText.value;
+    var APICall = "http://api.openweathermap.org/geo/1.0/direct?q=" +currentCity+ "&limit=1&appid=f4ce1d986e6fe51c9fed10dd756f803b"
+    if (currentCity === ""){
+        alert("You must type in a city name");
+        return;
+    }
+    getAPIData(APICall);
+
+    citySearchText.value = "";
+    
+    
+
+}
+
+// getAPI(requestURL);
+
+searchButton.addEventListener("click", searchForCity);
