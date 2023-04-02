6a57f03a3328f68ed386d5bf454b08eb3be56d85
Williamatthewood <w.wood@tcu.edu>
2023-04-02 15:56:52 -0500

    got first fetch request to work to get lat and lon of given city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
-var APIKey = "f4ce1d986e6fe51c9fed10dd756f803b"
+var APIKey = "f4ce1d986e6fe51c9fed10dd756f803b";
+var requestURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=f4ce1d986e6fe51c9fed10dd756f803b";
 
-console.log("test");
+var currentCity = document.getElementById("current-city");
+var currentDate = document.getElementById("current-date");
+var currentTemp = document.getElementById("current-temp");
+var currentWind = document.getElementById("current-wind");
+var currentHumidity = document.getElementById("current-humidity");
+
+function getAPI(request){
+    fetch(request).then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+}
+
+getAPI(requestURL);
